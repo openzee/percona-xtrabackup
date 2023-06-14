ba0e0925accd709a535e227ba997dbab398f0484
openzee <zgxie@aibee.com>
2023-06-14 19:02:48 +0800

    调整镜像名

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,9 @@
 #########################################################################
 # File Name: build_docker.sh
 # Created Time: 2023-06-14 18:41:12
-# Last modified: 2023-06-14 18:41:41
+# Last modified: 2023-06-14 19:00:39
 #########################################################################
 #!/bin/bash
 
-docker build -t harbor.aibee.cn/platform/xtrabackup:2.4.26.my  -f Dockerfile_for_my_xtrabackup .
+docker build -t platform/xtrabackup:2.4.26.my  -f Dockerfile_for_my_xtrabackup .
 
